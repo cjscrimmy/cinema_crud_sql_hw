2bffd63c433284623e4f5280f1eff04f3cf6f8df
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-13 17:11:17 +0100

    add sql for question 8 and include result

--- marvel.sql
+++ marvel.sql
@@ -188,8 +188,8 @@ INSERT INTO movies (title, year, show_time) VALUES ('Black Panther', 2018, '21:0
 -- (17 rows)
 
 -- 7.  Eric Cartman has decided to hijack our movie evening, Remove him from the table of people.
-DELETE FROM people WHERE name = 'Eric Cartman';
-SELECT name FROM people;
+-- DELETE FROM people WHERE name = 'Eric Cartman';
+-- SELECT name FROM people;
 
 --result
 --  name         
@@ -210,5 +210,34 @@ SELECT name FROM people;
 --  Mayor Joe Quimby
 --  Groundskeeper Willie
 -- (15 rows)
+
 -- 8.  The cinema has just heard that they will be holding an exclusive midnight showing of 'Avengers: Infinity War'!! Create a new entry in the 'movies' table to reflect this.
+INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 2018, '00:00');
+SELECT * FROM movies;
+
+--result
+--  id |                title                | year | show_time 
+-- ----+-------------------------------------+------+-----------
+--   1 | Iron Man                            | 2008 | 17:00
+--   2 | The Incredible Hulk                 | 2008 | 23:55
+--   3 | Iron Man 2                          | 2010 | 18:45
+--   4 | Thor                                | 2011 | 15:45
+--   5 | Captain America: The First Avenger  | 2011 | 14:15
+--   6 | Avengers Assemble                   | 2012 | 14:45
+--   7 | Iron Man 3                          | 2013 | 21:55
+--   8 | Thor: The Dark World                | 2013 | 22:55
+--   9 | Batman Begins                       | 2005 | 13:40
+--  10 | Captain America: The Winter Soldier | 2014 | 18:25
+--  11 | Guardians of the Galaxy             | 2014 | 13:10
+--  12 | Avengers: Age of Ultron             | 2015 | 20:20
+--  13 | Ant-Man                             | 2015 | 13:00
+--  14 | Captain America: Civil War          | 2016 | 12:35
+--  15 | Doctor Strange                      | 2016 | 22:00
+--  16 | Guardians of the Galaxy 2           | 2017 | 14:05
+--  17 | Spider-Man: Homecoming              | 2017 | 23:00
+--  18 | Thor: Ragnarok                      | 2017 | 22:10
+--  19 | Black Panther                       | 2018 | 21:00
+--  20 | Avengers: Infinity War              | 2018 | 00:00
+-- (20 rows)
+
 -- 9.  The cinema would like to make the Iron Man movies a triple billing. Find out the show time of "Iron Man 2" and set the show time of "Iron Man 3" to start two hours later.
